1123feaccb09554932ed5ad20e1c43b0ea1f182b
Arif <arif@themascoteers.com>
2023-12-18 04:17:12 +1100

    done

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
                 "dataset_4200_871.csv",
                 "dataset_525_871.csv"
                 ]
-    datasets = ["dataset_525_871.csv"]
+    #datasets = ["dataset_525_871.csv"]
     sizes = [2, 5, 10, 15, 20]
-    sizes = [2, 5]
+    #sizes = [2, 5]
     tasks = []
     for dataset in datasets:
         for algorithm in algorithms:
